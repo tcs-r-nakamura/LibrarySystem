418001305880791941cb47921d7a1ee044ffd3ff
tcs-r-nakamura <rnakamura.tcs@gmail.com>
2025-07-29 09:05:21 +0900

    更新

---
+++ LibrarySystem/LibraryServices/LibrayItemManager.cs
@@ -0,0 +1,32 @@
+﻿using LibrarySystem.Bases;
+using LibrarySystem.Interfaces;
+
+namespace LibrarySystem.LibraryServices
+{
+    public class LibraryItemManager<T> : ILibraryItemManager<T> where T : LibraryItem
+    {
+        private readonly List<T> items;
+        private readonly Action<T> processAction;
+
+        public LibraryItemManager(List<T> initialItems, Action<T> process)
+        {
+            items = initialItems;
+            processAction = process;
+        }
+        
+        public T? FindById(int id)
+        {
+            return items.Find(item => item.Id == id);
+        }
+
+        public T? FindByTitle(string title)
+        {
+            return items.Find(item => item.Title == title);
+        }
+
+        public void Process(T item)
+        {
+            processAction(item);
+        }
+    }
+}
